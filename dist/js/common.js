827ffa492ea378776bf9bcb7c72b226f4cc06ff6
kif <sashaplakhotniy@gmail.com>
2019-01-17 00:45:36 +0200

    finished index

---
+++ dist/js/common.js
@@ -0,0 +1,17 @@
+$( document ).ready(function () {
+    var sizer = '.sizer4',
+        container = $('.gallery');
+    
+    container.imagesLoaded(function () {
+        container.masonry({
+            itemSelector: '.item-masonry',
+            columnWidth: sizer,
+            persentPosition: true
+        })
+    });
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        dots:true,
+        dotsEach: true,
+    });
+});
